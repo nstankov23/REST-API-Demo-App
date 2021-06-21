6da4f80f7360f7ff19a17360f6ca0aa32fed732c
Nikola Krasimirov Stankov <n.stankov23@gmail.com>
2021-06-21 21:46:56 +0300

    add endpoint for getting a book

---
+++ BookApp.Server/Features/Books/BooksController.cs
@@ -0,0 +1,23 @@
+﻿namespace BookApp.Server.Features.Books
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<BookModel>> GetBy(int id)
+        {
+            return new BookModel()
+            {
+                Title = "Clean Code",
+                Description = "Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way. Noted software expert Robert C. Martin presents a revolutionary paradigm with Clean Code: A Handbook of Agile Software Craftsmanship . ",
+                AuthorName = "Robert C. Martin",
+                ImageUrl = "https://images-na.ssl-images-amazon.com/images/I/81jRujEs6uL.jpg",
+                Price = 30
+            };
+        }
+    }
+}
---
+++ BookApp.Server/Features/Books/Models/BookModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BookApp.Server.Features.Books
+{
+    public class BookModel
+    {
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public double Price { get; set; }
+    }
+}
